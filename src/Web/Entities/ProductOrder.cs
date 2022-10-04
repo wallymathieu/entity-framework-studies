dd9689616fa11f8642cdf210d79bd1e0cf24ce39
Oskar Gewalli <wallymathieu@users.noreply.github.com>
2022-10-04 07:33:12 +0200

    disable warning

--- src/Web/Entities/ProductOrder.cs
+++ src/Web/Entities/ProductOrder.cs
@@ -6,7 +6,9 @@ public record ProductOrder
     ///
     public virtual int Id { get; init; }
     ///
+#pragma warning disable CS8618
     public virtual Order Order { get; init; }
     ///
     public virtual Product Product { get; init; }
+#pragma warning restore CS8618
 }
